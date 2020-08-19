3bfa8bd32871f170fa5bada0cbffb0a64ce43c25
zacharymollenhour <Zacharymmollenhour@gmail.com>
2020-08-19 07:08:55 -0400

    init Commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,16 +1,13 @@
 ﻿
-/* Change this file to get your personal Portfolio */
-
-// Your Summary And Greeting Section
-
+//Summary And Greeting Section
 import emoji from "react-easy-emoji";
 
 const greeting = {
-  /* Your Summary And Greeting Section */
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
-  subTitle: emoji("A passionate Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."),
-  resumeLink: "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing"
+  //Summary and Greeting
+  username: "Zachary Mollenhour",
+  title: "Hi all, I'm Zach",
+  subTitle: emoji("A passionate Software Developer 🚀 having an experience of building Web and backend applications with JavaScript / Reactjs / Nodejs / C++ / Python and some other cool libraries and frameworks."),
+  resumeLink: "https://drive.google.com/file/d/1yZkqn6F7dE6VGAGXH2yibxWGgNWNYcsH/view?usp=sharing"
 };
 
 // Your Social Media Link
